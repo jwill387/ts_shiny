31925957f37e30a62604d7fcb187c15712977e1e
jwill387 <98768301+jwill387@users.noreply.github.com>
2022-02-03 09:27:10 -0500

    mvp with time series

--- server.R
+++ server.R
@@ -9,7 +9,14 @@ server <- function(input, output) {
   })
 
   output$ts.plot <- renderPlot({
+    min.date <- input$selected_date_range[1]
+    max.date <- input$selected_date_range[2]
+
     plot.df <- sales_df[sales_df$scode == input$selected_store & sales_df$pcode == input$selected_sku, ]
+
+    plot.df <- plot.df[plot.df$date >= min.date, ]
+    plot.df <- plot.df[plot.df$date <= max.date, ]
+
     autoplot(plot.df)
   })
 }
--- ui.R
+++ ui.R
@@ -3,15 +3,23 @@ library(shiny)
 ui <- fluidPage(
   selectInput(
     inputId = "selected_store",
-    label = "Select Store",
+    label = "Select Store:",
     choices = unique(sales_df$scode)
   ),
 
   selectInput(
     inputId = "selected_sku",
-    label = "Select SKU",
+    label = "Select SKU:",
     choices = unique(sales_df$pcode)
   ),
+
+  dateRangeInput(
+    inputId = "selected_date_range",
+    label = "Select Date Range:",
+    min = min(sales_df$date),
+    max = max(sales_df$date),
+  ),
+
   plotOutput("ts.plot"),
   verbatimTextOutput("debug")
 )
